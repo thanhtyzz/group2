bb6b4c92d7d9fcd5391edd053ff49ac0621df642
thanhtyzz <118091100+thanhtyzz@users.noreply.github.com>
2023-05-05 12:58:26 +0700

    Update Sign_up_Process.py

--- Modules/Signup/Sign_up_Process.py
+++ Modules/Signup/Sign_up_Process.py
@@ -4,24 +4,26 @@ import Modules.Login.Login_View as loginview
 from tkinter import END, messagebox as mbox
 
 
-class Sign_up_Process:
-
+class Sign_up_Process: 
+    
+    # Hiện màn hình lựa chọn giữa đăng nhập (login) và đăng kí (signup)
     @staticmethod
     def login_button_handle(obj):
         obj.window.destroy()
         app = loginview.Login_View()
         app.window.mainloop()
 
-    @staticmethod
-    def signup_button_handle(obj):
+    @staticmethod 
+    def signup_button_handle(obj): 
         username = obj.username_entry.get()
         password = obj.password_entry.get()
         reenterpassword = obj.reenterpass_entry.get()
         api = Signup_Api.Signup_Api()
-        error = api.check_user_signup(username, password, reenterpassword)
+        error = api.check_user_signup(username,password,reenterpassword)
+        standard_pass = api.check_password_standard(password, username)
 
         if error == -1:
-            mbox.showerror('Warning', 'Invalid User Input')
+            mbox.showerror('Warning','Invalid User Input')
             obj.username_entry.delete(0, END)
             obj.password_entry.delete(0, END)
             obj.reenterpass_entry.delete(0, END)
@@ -37,9 +39,40 @@ class Sign_up_Process:
             obj.username_entry.delete(0, END)
             obj.password_entry.delete(0, END)
             obj.reenterpass_entry.delete(0, END)
-
+            
         else:
-            mbox.showinfo('Success', 'Account created successfully')
-            obj.username_entry.delete(0, END)
-            obj.password_entry.delete(0, END)
-            obj.reenterpass_entry.delete(0, END)
+            if standard_pass == 1:
+                mbox.showerror('Mật khẩu yếu', 'Kí tự quá ngắn')
+                obj.username_entry.delete(0, END)
+                obj.password_entry.delete(0, END)
+                obj.reenterpass_entry.delete(0, END)
+            if standard_pass == 2:
+                mbox.showerror('Mật khẩu chưa đủ tiêu chuẩn', 'Không có chữ cái viết hoa')
+                obj.username_entry.delete(0, END)
+                obj.password_entry.delete(0, END)
+                obj.reenterpass_entry.delete(0, END)
+            if standard_pass == 3:
+                mbox.showerror('Mật khẩu chưa đủ tiêu chuẩn', 'Không có chữ cái viết thường')
+                obj.username_entry.delete(0, END)
+                obj.password_entry.delete(0, END)
+                obj.reenterpass_entry.delete(0, END)
+            if standard_pass == 4:
+                mbox.showerror('Mật khẩu chưa đủ tiêu chuẩn', 'Cần thêm kí tự đặc biệt')
+                obj.username_entry.delete(0, END)
+                obj.password_entry.delete(0, END)
+                obj.reenterpass_entry.delete(0, END)
+            if standard_pass == 5:
+                mbox.showerror('Mật khẩu chưa đủ tiêu chuẩn', 'Cần thêm kí tự chữ cái')
+                obj.username_entry.delete(0, END)
+                obj.password_entry.delete(0, END)
+                obj.reenterpass_entry.delete(0, END)
+            if standard_pass == 6:
+                mbox.showerror('Mật khẩu chưa đủ tiêu chuẩn', 'Cần thêm chữ số')
+                obj.username_entry.delete(0, END)
+                obj.password_entry.delete(0, END)
+                obj.reenterpass_entry.delete(0, END)
+            else:
+                mbox.showinfo('Success', 'Account created successfully')
+                obj.username_entry.delete(0, END)
+                obj.password_entry.delete(0, END)
+                obj.reenterpass_entry.delete(0, END)
