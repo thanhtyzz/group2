8217d74bdc193f1c510f95e117113d1039bf381e
thanhtyzz <118091100+thanhtyzz@users.noreply.github.com>
2023-05-05 13:02:11 +0700

    Update Admin_Inventory_process.py

--- Modules/Admin/Component/Inventory/Admin_Inventory_process.py
+++ Modules/Admin/Component/Inventory/Admin_Inventory_process.py
@@ -10,107 +10,70 @@ class Admin_Inventory_Process:
 
     @staticmethod
     def get_json_data(obj):
-        product_name = obj.product_name_entry.get()
-        description = obj.description_entry.get()
-        category = obj.category_entry.get()
+        film = obj.film_entry.get()
+        genre = obj.genre_entry.get()
         price = obj.price_entry.get()
+        showtime = obj.showtime_entry.get()
         current_stock = obj.current_stock_entry.get()
         add_stock = obj.add_stock_entry.get()
-        if product_name == '' or description == '' or category == '' or price == '' or current_stock == '' or add_stock == '':
+        if film == '' or genre == '' or showtime == '' or price == '' or current_stock == '' or add_stock == '':
             return 0
         else:
             json_data = {
-                'Product_name': f'{product_name}',
-                'Description' : f'{description}',
-                'Category': f'{category}',
+                'Film': f'{film}',
+                'Genre': f'{genre}',
                 'Price': f'{price}',
-                'Stock': f'{int(current_stock) + int(add_stock)}',
-            }
+                'Showtime': f'{showtime}',
+                'Stock': f'{int(current_stock) + int(add_stock)}',}
             return json_data
 
     @staticmethod
     def update_button_handle(obj):
         data = Admin_Inventory_Process.get_json_data(obj)
         if data == 0:
-            mbox.showerror('Error','Invalid User Input')
+            mbox.showerror('Error', 'Missing input data')
         else:
-            #get the product id selected in treeview
-            product_id = obj.tree.item(obj.tree.selection())['values'][0]
+            # Get the ID of the selected film in the treeview
+            film_id = obj.tree.item(obj.tree.selection())['values'][0]
             api = AdminApi.Admin_Api()
-            check = api.update_items(data, product_id)
+            check = api.update_items(data, film_id)
             if check == -2:
-                mbox.showerror('Error','Product id is not in collection but all information is in collection')
+                mbox.showerror('Error', 'Film ID already exists in the collection')
+            elif check == -3:
+                mbox.showerror('Error', 'Incorrect Showtime format')
+            elif check == -1:
+                mbox.showinfo('Info', 'No new information was provided for update')
             else:
-                for item in obj.tree.get_children():
-                    obj.tree.delete(item)
+                obj.tree.delete(*obj.tree.get_children())
                 table_data = api.get_all_warehouse_data()
                 for row in table_data:
-                    obj.tree.insert('', 'end', values = (row['Product_id'], row['Product_name'], row['Description'], row['Category'], row['Price'], row['Stock']))
-                mbox.showinfo('Success','Updated Succesfully')
-                obj.current_stock_entry.config(state = NORMAL)
-                obj.product_name_entry.delete(0, END)
-                obj.description_entry.delete(0, END)
-                obj.category_entry.delete(0, END)
+                    obj.tree.insert('', 'end', values=(
+                    row['Film_ID'], row['Film'], row['Genre'], row['Showtime'], row['Price'], row['Stock']))
+                mbox.showinfo('Success', 'Update process completed')
+                obj.current_stock_entry.config(state=NORMAL)
+                obj.film_entry.delete(0, END)
+                obj.genre_entry.delete(0, END)
+                obj.showtime_entry.delete(0, END)
                 obj.price_entry.delete(0, END)
                 obj.current_stock_entry.delete(0, END)
                 obj.add_stock_entry.delete(0, END)
-                obj.current_stock_entry.config(state = DISABLED)
+                obj.current_stock_entry.config(state=DISABLED)
 
     @staticmethod
     def remove_button_handle(obj):
         api = AdminApi.Admin_Api()
-        #get the product id selected in treeview
-        product_id = obj.tree.item(obj.tree.selection())['values'][0]
-        check = api.remove_items(product_id)
-        mbox.showinfo('Success','Removed successfully')
+        # Get the ID of the selected film in the treeview
+        film_id = obj.tree.item(obj.tree.selection())['values'][0]
+        check = api.remove_items(film_id)
+        mbox.showinfo('Success', 'Update process completed')
         for item in obj.tree.get_children():
             obj.tree.delete(item)
         table_data = api.get_all_warehouse_data()
         for row in table_data:
-            obj.tree.insert('', 'end', values = (row['Product_id'], row['Product_name'], row['Description'], row['Category'], row['Price'], row['Stock']))
-        obj.product_name_entry.delete(0, END)
-        obj.description_entry.delete(0, END)
-        obj.category_entry.delete(0, END)
+            obj.tree.insert('', 'end', values=(row['Film_ID'], row['Film'], row['Genre'], row['Showtime'], row['Price'], row['Stock']))
+        obj.film_entry.delete(0, END)
+        obj.genre_entry.delete(0, END)
+        obj.showtime_entry.delete(0, END)
         obj.price_entry.delete(0, END)
         obj.current_stock_entry.delete(0, END)
         obj.add_stock_entry.delete(0, END)
-
-    @staticmethod
-    def reset(obj):
-        obj.product_name_entry.delete(0, END)
-        obj.description_entry.delete(0, END)
-        obj.category_entry.delete(0, END)
-        obj.price_entry.delete(0, END)
-        obj.current_stock_entry.delete(0, END)
-        obj.add_stock_entry.delete(0, END)
-
-    @staticmethod
-    def search_button_handle(obj):
-        api = AdminApi.Admin_Api()
-        search_term = obj.search_entry.get()
-        if search_term == '':
-            mbox.showerror('Error','Invalid User Input')
-        else:
-            for item in obj.tree.get_children():
-                obj.tree.delete(item)
-            data  = api.warehouse_collection.find({'Product_name': {'$regex': f'{search_term}', '$options': 'i'}})
-            for row in data:
-                obj.tree.insert('', 'end', values = (row['Product_id'], row['Product_name'], row['Description'], row['Category'], row['Price'], row['Stock']))
-
-    @staticmethod
-    def reset_button_handle(obj):
-        api = AdminApi.Admin_Api()
-        for item in obj.tree.get_children():
-            obj.tree.delete(item)
-        table_data = api.get_all_warehouse_data()
-        for row in table_data:
-            obj.tree.insert('', 'end', values = (row['Product_id'], row['Product_name'], row['Description'], row['Category'], row['Price'], row['Stock']))
-        obj.current_stock_entry.config(state = NORMAL)
-        obj.search_entry.delete(0, END)
-        obj.product_name_entry.delete(0, END)
-        obj.description_entry.delete(0, END)
-        obj.category_entry.delete(0, END)
-        obj.price_entry.delete(0, END)
-        obj.current_stock_entry.delete(0, END)
-        obj.add_stock_entry.delete(0, END)
-        obj.current_stock_entry.config(state = DISABLED)
