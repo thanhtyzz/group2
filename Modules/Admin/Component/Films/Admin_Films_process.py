ca7bcebcadf5a756d097ef6d73ed71926d8bae3f
NguyenLePhuongAnk <129772748+NguyenLePhuongAnk@users.noreply.github.com>
2023-05-05 15:08:30 +0700

    Update Admin_Films_process.py

--- Modules/Admin/Component/Films/Admin_Films_process.py
+++ Modules/Admin/Component/Films/Admin_Films_process.py
@@ -4,39 +4,65 @@ import json
 import Api.Admin_Api as admin_api
 
 
-class Admin_Products_Process:
+class Admin_Films_Process:
 
     @staticmethod
-    def add_item_button_handle(obj):
+    def addfilm_button_handle(obj):
         api = admin_api.Admin_Api()
-        # Get all data from products form
-        product_id = obj.product_id_entry.get()
-        product_name = obj.product_name_entry.get()
+        # Get all data from films form
+        film_id = obj.film_id_entry.get()
+        film = obj.film_entry.get()
         description = obj.description_entry.get()
         category = obj.category_entry.get()
         price = obj.price_entry.get()
         stock = obj.stock_entry.get()
-        if product_id == "" or product_name == "" or category == "" or price == "" or stock == "":
+        if film_id == "" or film == "" or category == "" or price == "" or stock == "":
             messagebox.showerror("Error", "Invalid Input")
         else:
-            json_data = {"Product_id": f"{product_id}", "Product_name": f"{product_name}",
+            json_data = {"Film_id": f"{film_id}", "Film": f"{film}",
                          "Description": f"{description}", "Category": f"{category}",
                          "Price": float(f"{price}"), "Stock": int(f"{stock}")}
             check = api.add_new_item(json_data)
             if check == -1:
-                messagebox.showinfo("Error", "Product id is not in collection but all information is in collection")
+                messagebox.showinfo("Error", "Film id is not in collection but all information is in collection")
             elif check == 0:
                 messagebox.showinfo("Success", "Inserted Succesfully")
             else:
-                messagebox.showinfo("Error", "Product already existed")
+                messagebox.showinfo("Error", "Film already existed")
 
     @staticmethod
     def reset_button_handle(obj):
         api = admin_api.Admin_Api()
         new_prod_id = api.get_last_prod_id()
-        obj.product_id.set(new_prod_id)
-        obj.product_name_entry.delete(0, END)
+        obj.film_id.set(new_prod_id)
+        obj.film_entry.delete(0, END)
         obj.description_entry.delete(0, END)
         obj.category_entry.delete(0, END)
         obj.price_entry.delete(0, END)
         obj.stock_entry.delete(0, END)
+    
+    @staticmethod
+    def click_button(self,button):
+        if button == "films": 
+            apc.Admin_Films_create.generate_films(self) 
+        elif button == "inventory":
+            aic.Admin_Inventory_create.generate_inventory(self)
+        elif button == "sales": 
+            asc.Admin_Sales_create.generate_sales(self) 
+        elif button == "users":
+            auc.Admin_User_create.generate_users(self) 
+
+    def exit_button(self): 
+            if messagebox.askyesno("Quit", "Are you sure you want to quit?"): 
+                self.window.destroy() 
+                exit() 
+            else: 
+                return
+        
+    def switch_account(self): 
+        if messagebox.askyesno("Change Account", "Are you sure you want to change account?"): 
+            self.window.destroy() 
+            app = lgv.Login_View()
+            app.window.mainloop() 
+        else:
+            return
