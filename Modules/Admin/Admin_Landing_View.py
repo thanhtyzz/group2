d131dc7c9cad7c0948b87376832f35c972751627
thanhtyzz <118091100+thanhtyzz@users.noreply.github.com>
2023-05-05 12:29:59 +0700

    Update Admin_Landing_View.py

--- Modules/Admin/Admin_Landing_View.py
+++ Modules/Admin/Admin_Landing_View.py
@@ -1,79 +1,55 @@
 from tkinter import *
-import sys
-sys.path.append("C:\\Users\\User\\PycharmProjects\\FinalTerm\\Modules\\Admin\\Admin_Landing_Process")
-import Admin_Landing_Process as ap
+from pathlib import Path
+import Modules.Admin.Admin_Landing_Process as ap
 
 
 class Admin_View:
-
     def __init__(self):
         self.window = Tk()
-
         # get screen width and height
         self.screen_width = self.window.winfo_screenwidth()
         self.screen_height = self.window.winfo_screenheight()
 
         # set window width and height
-        self.window_width = 1080
-        self.window_height = 720
+        self.window_width = 685
+        self.window_height = 492
         # set window position
         self.window.geometry("%dx%d+%d+%d" % (self.window_width, self.window_height,
-                                              (self.screen_width - self.window_width) / 2,
-                                              (self.screen_height - self.window_height) / 2))
+                             (self.screen_width - self.window_width) / 2, (self.screen_height - self.window_height) / 2))
+        self.window.configure(bg="#FFFFFF")
+        self.window.title("Admin")
 
-        self.window.configure(bg="#ffffff")
-        self.window.title('Admin')
-        self.window.iconphoto(False, PhotoImage(
-            file=f"./Images/User/MainPage/UserIcon.png"))
-
-        self.canvas = Canvas(self.window, bg="#ffffff", height=720, width=1080,
-                             bd=0, highlightthickness=0, relief="ridge")
-
+        self.canvas = Canvas(self.window, bg="#FFFFFF", height=492, width=685, bd=0, highlightthickness=0, relief="ridge")
         self.canvas.place(x=0, y=0)
 
-        # -----background-----
-        self.background_img = PhotoImage(file=f"./Images/Admin/LandingPage/Background.png")
-        self.background = self.canvas.create_image(565, 375, image=self.background_img)
+        assets_path = Path(r"D:\do-an-cuoi-ki-nhom2\Images\Admin\LandingPage")
 
-        # -----button-log-out-----
-        self.img_log_out = PhotoImage(file=f"./Images/Admin/LandingPage/Button_Logout.png")
-        self.button_log_out = Button(image=self.img_log_out, borderwidth=0,
-                                     highlightthickness=0, relief="flat", bg="#ffffff", activebackground="#ffffff",
-                                     command=lambda: ap.Admin_Process.log_out_button_handle(self))
+        self.background_img = PhotoImage(file=assets_path / "Background.png")
+        self.logout_image = PhotoImage(file=assets_path / "Button_Logout.png")
+        self.films_image = PhotoImage(file=assets_path / "Button_Films.png")
+        self.inventory_image = PhotoImage(file=assets_path / "Button_Inventory.png")
+        self.sales_image = PhotoImage(file=assets_path / "Button_Sales.png")
+        self.users_image = PhotoImage(file=assets_path / "Button_Users.png")
 
-        self.button_log_out.place(x=800, y=60, width=210, height=60)
+        self.background = self.canvas.create_image(342.0, 246.0, image=self.background_img)
 
-        # -----button-products-----
-        self.img_products = PhotoImage(file=f"./Images/Admin/LandingPage/Button_Products.png")
-        self.button_products = Button(image=self.img_products, borderwidth=0,
-                                      highlightthickness=0, relief="flat", bg="#ffffff", activebackground="#ffffff",
-                                      command=lambda: ap.Admin_Process.products_button_handle(self))
+        self.logout_button = Button(image=self.logout_image, borderwidth=0, highlightthickness=0,
+                               command=lambda: ap.Admin_Process.log_out_button_handle(self))
+        self.logout_button.place(x=564.0, y=19.0, width=101.0, height=36.0)
 
-        self.button_products.place(x=49, y=582, width=170, height=55)
+        self.films_button = Button(image=self.films_image, borderwidth=0, highlightthickness=0,
+                               command=lambda: ap.Admin_Process.films_button_handle(self))
+        self.films_button.place(x=51.0, y=343.0, width=101.0, height=36.0)
 
-        # -----button-inventory-----
-        self.img_inventory = PhotoImage(file=f"./Images/Admin/LandingPage/Button_Inventory.png")
+        self.inventory_button = Button(image=self.inventory_image, borderwidth=0, highlightthickness=0,
+                               command=lambda: ap.Admin_Process.inventory_button_handle(self))
+        self.inventory_button.place(x=208.0, y=341.0, width=101.0, height=36.0)
 
-        self.button_inventory = Button(image=self.img_inventory, borderwidth=0,
-                                       highlightthickness=0, relief="flat", bg="#ffffff", activebackground="#ffffff",
-                                       command=lambda: ap.Admin_Process.inventory_button_handle(self))
+        self.sales_button = Button(image=self.login_image_2, borderwidth=0, highlightthickness=0,
+                               command=lambda: ap.Admin_Process.sales_button_handle(self))
+        self.sales_button.place(x=51.0, y=400.0, width=101.0, height=36.0)
 
-        self.button_inventory.place(x=219, y=582, width=170, height=55)
-
-        # -----button-sales-----
-        self.img_sales = PhotoImage(file=f"./Images/Admin/LandingPage/Button_Sales.png")
-        self.button_sales = Button(image=self.img_sales, borderwidth=0, highlightthickness=0,
-                                   relief="flat", bg="#ffffff", activebackground="#ffffff",
-                                   command=lambda: ap.Admin_Process.sales_button_handle(self))
-
-        self.button_sales.place(x=389, y=582, width=170, height=55)
-
-        # -----button-users-----
-        self.img_users = PhotoImage(file=f"./Images/Admin/LandingPage/Button_Users.png")
-        self.button_users = Button(image=self.img_users, borderwidth=0, highlightthickness=0,
-                                   relief="flat", bg="#ffffff", activebackground="#ffffff",
-                                   command=lambda: ap.Admin_Process.users_button_handle(self))
-
-        self.button_users.place(x=559, y=582, width=170, height=55)
-
-        self.window.resizable(False, False)
+        self.users_button = Button(image=self.users_image, borderwidth=0, highlightthickness=0,
+                               command=lambda: ap.Admin_Process.users_button_handle(self))
+        self.users_button.place(x=208.0, y=402.0, width=101.0, height=36.0)
+        self.window.resizable(0, 0)
