5428d733d15f82bd2306f9b0de3c4714dcbeb079
NguyenLePhuongAnk <129772748+NguyenLePhuongAnk@users.noreply.github.com>
2023-05-06 00:56:43 +0700

    Update User_Films_process.py

--- Modules/User/Component/Films/User_Films_process.py
+++ Modules/User/Component/Films/User_Films_process.py
@@ -2,14 +2,14 @@ from tkinter import *
 import Api.User_Api as Api
 from tkinter import ttk
 
-class User_Items_Process:
+class User_Films_Process:
 
     @staticmethod
     def generate_films_table(obj):
         api = Api.User_Api()
         data = api.get_all_warehouse_data()
         #create a tree view in table frame
-        tree = ttk.Treeview(obj.tableframe, columns=("Film id","Film", "Genre", "Showtime", "Price", "Stock"), height = 20)
+        tree = ttk.Treeview(obj.tableframe, columns=("Film id","Film", "Genre", "Showtime", "Price", "Stock"), height = 10)
         tree.heading("#0")
         tree.heading("#1", text = "Film id")
         tree.heading("#2", text = "Film")
@@ -17,13 +17,13 @@ class User_Items_Process:
         tree.heading("#4", text = "Showtime")
         tree.heading("#5", text = "Price")
         tree.heading("#6", text = "Stock")
-        tree.column("#0", width = 160, stretch = NO)
-        tree.column("#1", width = 160, stretch = NO)
-        tree.column("#2", width = 160, stretch = NO)
-        tree.column("#3", width = 160, stretch = NO)
-        tree.column("#4", width = 160, stretch = NO)
-        tree.column("#5", width = 160, stretch = NO)
-        tree.column("#6", width = 160, stretch = NO)
+        tree.column("#0", width = 80, stretch = NO)
+        tree.column("#1", width = 80, stretch = NO)
+        tree.column("#2", width = 80, stretch = NO)
+        tree.column("#3", width = 80, stretch = NO)
+        tree.column("#4", width = 80, stretch = NO)
+        tree.column("#5", width = 80, stretch = NO)
+        tree.column("#6", width = 80, stretch = NO)
 
         tree.grid(row = 0, column = 0, columnspan = 7, sticky = (N, S, W, E))
         scrollbarx = Scrollbar(obj.tableframe, orient = HORIZONTAL, command=tree.xview())
