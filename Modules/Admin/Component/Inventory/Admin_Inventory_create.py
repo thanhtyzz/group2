e0e30dcc7767881389d97cba708820e066eb9190
NguyenLePhuongAnk <129772748+NguyenLePhuongAnk@users.noreply.github.com>
2023-05-05 15:39:38 +0700

    Update Admin_Inventory_create.py

--- Modules/Admin/Component/Inventory/Admin_Inventory_create.py
+++ Modules/Admin/Component/Inventory/Admin_Inventory_create.py
@@ -5,6 +5,8 @@ import Service.Widget_service as ws
 import Modules.Admin.Component.Inventory.Admin_Inventory_process as aip
 
 class Admin_Inventory_create:
+    
+    @staticmethod
     def __init__(self):
         self.window = Tk()
         # get screen width and height
@@ -23,10 +25,10 @@ class Admin_Inventory_create:
         self.canvas = Canvas(self.window, bg="#FFFFFF", height=492, width=685, bd=0, highlightthickness=0, relief="ridge")
         self.canvas.place(x=0, y=0)
 
-        assets_path = Path(r"D:\Study\HK2\KY THUAT LAP TRINH\Do_an-cuoi_ki_Nhom2\File\Images\Admin\Inventory")
+        assets_path = Path(r"D:\do-an-cuoi-ki-nhom-2\Images\Admin\Component\Inventory")
 
         self.background_img = PhotoImage(file=assets_path / "Background.png")
-        self.movies_image = PhotoImage(file=assets_path / "Button_Movies.png")
+        self.films_image = PhotoImage(file=assets_path / "Button_Films.png")
         self.inventory_image = PhotoImage(file=assets_path / "Button_Inventory.png")
         self.sales_image = PhotoImage(file=assets_path / "Button_Sales.png")
         self.users_image = PhotoImage(file=assets_path / "Button_Users.png")
@@ -41,28 +43,28 @@ class Admin_Inventory_create:
 
         self.background = self.background_img.create_image(342.0, 246.0, image=self.background_img)
 
-        self.movies_button = Button(image=self.movies_image, borderwidth=0, highlightthickness=0,
-                               command=lambda: aip.Admin_Inventory_process.movies_button_handle(self))
-        self.movies_button.place(x=10.0, y=124.0, width=97.0, height=37.0)
+        self.films_button = Button(image=self.films_image, borderwidth=0, highlightthickness=0,
+                               command=lambda: self.click_button("films"))
+        self.films_button.place(x=10.0, y=124.0, width=97.0, height=37.0)
 
         self.inventory_button = Button(image=self.inventory_image, borderwidth=0, highlightthickness=0,
-                               command=lambda: aip.Admin_Inventory_process.inventory_button_handle(self))
+                               command=lambda: self.click_button("inventory"))
         self.inventory_button.place(x=118.0, y=125.0, width=102.0, height=35.0)
 
         self.sales_button = Button(image=self.sales_image, borderwidth=0, highlightthickness=0,
-                               command=lambda: aip.Admin_Inventory_process.sales_button_handle(self))
+                               command=lambda: self.click_button("sales"))
         self.sales_button.place(x=233.0, y=126.0, width=97.0, height=33.0)
 
         self.users_button = Button(image=self.users_image, borderwidth=0, highlightthickness=0,
-                               command=lambda: aip.Admin_Inventory_process.users_button_handle(self))
+                               command=lambda: self.click_button("users"))
         self.users_button.place(x=349.0, y=126.0, width=97.0, height=33.0)
 
         self.switch_button = Button(image=self.switch_image, borderwidth=0, highlightthickness=0,
-                               command=lambda: aip.Admin_Inventory_process.switch_button_handle(self))
+                               command=lambda: self.switch_account())
         self.switch_button.place(x=461.0, y=126.0, width=97.0, height=33.0)
 
         self.exit_button = Button(image=self.exit_image, borderwidth=0, highlightthickness=0,
-                               command=lambda: aip.Admin_Inventory_process.exit_button_handle(self))
+                               command=lambda: self.exit_button())
         self.exit_button.place(x=576.0, y=126.0, width=97.0, height=33.0)
 
         self.update_button = Button(image=self.update_image, borderwidth=0, highlightthickness=0,
@@ -118,14 +120,30 @@ class Admin_Inventory_create:
         self.reset_button.place(x=603.0, y=185.0, width=61.0, height=21.0)
         self.window.resizable(0, 0)
 
-         # create tree view
-        obj.tree = ttk.Treeview(obj.tableframe, columns = ('ID', 'Name', 'Description',
-                                                         'Category', 'Price', 'Stock'))
+    @staticmethod
+    def generate_inventory_table(obj):
+        def clickprodtable(event):
+            # get selected film
+            cur = obj.tree.selection()
+            cur = obj.tree.item(cur)
+            try:
+                obj.selected_film = cur['values']
+                obj.film.set(cur['values'][1])
+                obj.genre.set(cur['values'][2])
+                obj.showtime.set(cur['values'][3])
+                obj.price.set(cur['values'][4])
+                obj.current_stock.set(cur['values'][5])
+            except:
+                pass
+
+        # create tree view
+        obj.tree = ttk.Treeview(obj.tableframe, columns = ('ID', 'Name', 'Genre',
+                                                         'Showtime', 'Price', 'Stock'))
         obj.tree.heading('#0')
         obj.tree.heading('#1', text ='ID') 
         obj.tree.heading('#2', text = 'Name')
-        obj.tree.heading('#3', text = 'Description')
-        obj.tree.heading('#4', text = 'Category')
+        obj.tree.heading('#3', text = 'Genre')
+        obj.tree.heading('#4', text = 'Showtime')
         obj.tree.heading('#5', text = 'Price')
         obj.tree.heading('#6', text = 'Stock')
 
@@ -144,7 +162,7 @@ class Admin_Inventory_create:
         obj.scrollbary.grid(row = 1, column = 6)
 
         api = Api.Admin_Api()
-        products = api.get_all_warehouse_data()
+        films = api.get_all_warehouse_data()
 
-        for product in products: 
-            obj.tree.insert('', 'end', values = (product['Product_id'], product['Product_name'], product['Description'], product['Category'], product['Price'], product['Stock']))
+        for film in films: 
+            obj.tree.insert('', 'end', values = (film['film_id'], film['film'], film['Genre'], film['Showtime'], film['Price'], film['Stock']))
