8230a355224a8d01b4c475f2a0f153569087c1e8
DuyenAn <antvd22411c@st.uel.edu.vn>
2023-05-07 09:19:32 +0700

    Message

--- Modules/Admin/Component/Admin_Main_View.py
+++ Modules/Admin/Component/Admin_Main_View.py
@@ -1,115 +1,93 @@
 from tkinter import *
+from pathlib import Path
 from tkinter import messagebox
 import Modules.Admin.Component.Films.Admin_Films_create as apc
 import Modules.Admin.Component.Inventory.Admin_Inventory_create as aic
-import Modules.Admin.Component.Sales.Admin_Sales_create as asc
-import Modules.Admin.Component.Users.Admin_User_create as auc
+import Modules.Admin.Component.Sales.Admin_Sales_create as asc 
+import Modules.Admin.Component.Users.Admin_User_create as auc 
 import Modules.Login.Login_View as lgv
 
 
 class Admin_Main_View:
-
+    
     def __init__(self):
-
         self.window = Tk()
-
         # get screen width and height
         self.screen_width = self.window.winfo_screenwidth()
         self.screen_height = self.window.winfo_screenheight()
 
         # set window width and height
-        self.window_width = 1080
-        self.window_height = 720
+        self.window_width = 685
+        self.window_height = 492
         # set window position
         self.window.geometry("%dx%d+%d+%d" % (self.window_width, self.window_height,
-                                              (self.screen_width - self.window_width) / 2,
-                                              (self.screen_height - self.window_height) / 2))
-
-        self.window.configure(bg="#ffffff")
-        self.window.title('Admin')
-        self.window.iconphoto(False, PhotoImage(
-            file=f"./Images/User/MainPage/UserIcon.png"))
+                             (self.screen_width - self.window_width) / 2, (self.screen_height - self.window_height) / 2))
+        self.window.configure(bg="#FFFFFF")
+        self.window.title("Admin")
 
         self.allframes = []
 
-        self.canvas = Canvas(self.window, bg="#ffffff", height=720, width=1080,
-                             bd=0, highlightthickness=0, relief="ridge")
+        self.canvas = Canvas(self.window, bg="#FFFFFF", height=492, width=685, bd=0, highlightthickness=0, relief="ridge")
         self.canvas.place(x=0, y=0)
 
-        # -----background-----
-        self.img_background = PhotoImage(file=f"./Images/Admin/MainPage/background.png")
-        self.background = self.canvas.create_image(540, 65, image=self.img_background)
+        assets_path = Path(r"D:\do-an-cuoi-ki-nhom-2\Images\Admin\MainPage")
 
-        # -----button-products-----
-        self.img_products = PhotoImage(file=f"./Images/Admin/MainPage/img_products.png")
-        self.button_products = Button(image=self.img_products, borderwidth=0,
-                                      highlightthickness=0, relief="flat", bg="#ffffff", activebackground="#ffffff",
-                                      command=lambda: self.click_button("products"))
+        self.background_img = PhotoImage(file=assets_path / "Background.png")
+        self.films_image = PhotoImage(file=assets_path / "Button_Films.png")
+        self.inventory_image = PhotoImage(file=assets_path / "Button_Inventory.png")
+        self.sales_image = PhotoImage(file=assets_path / "Button_Sales.png")
+        self.users_image = PhotoImage(file=assets_path / "Button_Users.png")
+        self.switch_image = PhotoImage(file=assets_path / "Button_Switch.png")
+        self.exit_image = PhotoImage(file=assets_path / "Button_Exit.png")
 
-        self.button_products.place(x=30, y=160, width=155, height=60)
+        self.background = self.canvas.create_image(342.0, 246.0, image=self.background_img)
 
-        # -----button-inventory-----
-        self.img_inventory = PhotoImage(file=f"./Images/Admin/MainPage/img_inventory.png")
-        self.button_inventory = Button(image=self.img_inventory, borderwidth=0,
-                                       highlightthickness=0, relief="flat", bg="#ffffff", activebackground="#ffffff",
-                                       command=lambda: self.click_button("inventory"))
+        self.films_button = Button(image=self.films_image, borderwidth=0, highlightthickness=0,
+                               command=lambda: self.click_button("films"), relief="flat")
+        self.films_button.place(x=10.0, y=124.0, width=97.0, height=37.0)
 
-        self.button_inventory.place(x=200, y=160, width=155, height=60)
+        self.inventory_button = Button(image=self.inventory_image, borderwidth=0, highlightthickness=0,
+                               command=lambda: self.click_button("inventory"), relief="flat")
+        self.inventory_button.place(x=118.0, y=125.0, width=102.0, height=35.0)
 
-        # -----button-sales-----
-        self.img_sales = PhotoImage(file=f"./Images/Admin/MainPage/img_sales.png")
-        self.button_sales = Button(image=self.img_sales, borderwidth=0,
-                                   highlightthickness=0, relief="flat", bg="#ffffff", activebackground="#ffffff",
-                                   command=lambda: self.click_button("sales"))
+        self.sales_button = Button(image=self.sales_image, borderwidth=0, highlightthickness=0,
+                               command = self.click_button("sales"), relief="flat")
+        self.sales_button.place(x=233.0, y=126.0, width=97.0, height=33.0)
 
-        self.button_sales.place(x=380, y=160, width=155, height=60)
+        self.users_button = Button(image=self.users_image, borderwidth=0, highlightthickness=0,
+                               command = self.click_button("users"), relief="flat")
+        self.users_button.place(x=349.0, y=126.0, width=97.0, height=33.0)
 
-        # -----button-users-----
-        self.img_users = PhotoImage(file=f"./Images/Admin/MainPage/img_users.png")
-        self.button_users = Button(image=self.img_users, borderwidth=0,
-                                   highlightthickness=0, relief="flat", bg="#ffffff", activebackground="#ffffff",
-                                   command=lambda: self.click_button("users"))
+        self.switch_button = Button(image=self.switch_image, borderwidth=0, highlightthickness=0,
+                               command = self.switch_account(), relief="flat")
+        self.switch_button.place(x=461.0, y=126.0, width=97.0, height=33.0)
 
-        self.button_users.place(x=550, y=160, width=155, height=60)
+        self.exit_button = Button(image=self.exit_image, borderwidth=0, highlightthickness=0,
+                               command = self.exit(), relief="flat")
+        self.exit_button.place(x=576.0, y=126.0, width=97.0, height=33.0)
+        self.window.resizable(0, 0)
 
-        # -----button-switch-account-----
-        self.img_switch = PhotoImage(file=f"./Images/Admin/MainPage/img_switch.png")
-        self.button_switch = Button(image=self.img_switch, borderwidth=0,
-                                    highlightthickness=0, relief="flat", bg="#ffffff", activebackground="#ffffff",
-                                    command=lambda: self.switch_account())
-
-        self.button_switch.place(x=730, y=160, width=155, height=60)
-
-        # -----button-exit-----
-        self.img_exit = PhotoImage(file=f"./Images/Admin/MainPage/img_exit.png")
-        self.button_exit = Button(image=self.img_exit, borderwidth=0,
-                                  highlightthickness=0, relief="flat", bg="#ffffff", activebackground="#ffffff",
-                                  command=lambda: self.exit_button())
-
-        self.button_exit.place(x=900, y=160, width=155, height=60)
-        self.window.resizable(False, False)
-
-    def click_button(self, button):
-        if button == "products":
-            apc.Admin_Products_create.generate_products(self)
+    def click_button(self,button):
+        if button == "films": 
+            apc.Admin_Films_create.generate_films(self) 
         elif button == "inventory":
             aic.Admin_Inventory_create.generate_inventory(self)
-        elif button == "sales":
-            asc.Admin_Sales_create.generate_sales(self)
+        elif button == "sales": 
+            asc.Admin_Sales_create.generate_sales(self) 
         elif button == "users":
-            auc.Admin_User_create.generate_users(self)
+            auc.Admin_User_create.generate_users(self) 
 
-    def exit_button(self):
-        if messagebox.askyesno("Quit", "Are you sure you want to quit?"):
-            self.window.destroy()
-            exit()
+    def exit(self): 
+            if messagebox.askyesno("Quit", "Are you sure you want to quit?"): 
+                self.window.destroy() 
+                exit() 
+            else: 
+                return
+        
+    def switch_account(self): 
+        if messagebox.askyesno("Change Account", "Are you sure you want to change account?"): 
+            self.window.destroy() 
+            app = lgv.Login_View()
+            app.window.mainloop() 
         else:
             return
-
-    def switch_account(self):
-        if messagebox.askyesno("Change Account", "Are you sure you want to change account?"):
-            self.window.destroy()
-            app = lgv.Login_View()
-            app.window.mainloop()
-        else:
-            return 
--- Modules/User/Component/Films/User_Films_process.py
+++ Modules/User/Component/Films/User_Films_process.py
@@ -2,16 +2,16 @@ from tkinter import *
 import Api.User_Api as Api
 from tkinter import ttk
 
-class User_Films_Process:
+class User_Films_process:
 
     @staticmethod
     def generate_films_table(obj):
         api = Api.User_Api()
         data = api.get_all_warehouse_data()
         #create a tree view in table frame
-        tree = ttk.Treeview(obj.tableframe, columns=("Film_ID","Film", "Genre", "Showtime", "Price", "Stock"), height = 10)
+        tree = ttk.Treeview(obj.tableframe, columns=("Film id","Film", "Genre", "Showtime", "Price", "Stock"), height = 10)
         tree.heading("#0")
-        tree.heading("#1", text = "Film_ID")
+        tree.heading("#1", text = "Film id")
         tree.heading("#2", text = "Film")
         tree.heading("#3", text = "Genre")
         tree.heading("#4", text = "Showtime")
@@ -30,6 +30,6 @@ class User_Films_Process:
         scrollbary= Scrollbar(obj.tableframe, orient = VERTICAL, command=tree.yview())
         scrollbary.grid(row=0, column=7, sticky=(N, S, W, E))
 
-        # add data to tree view
+        #add data to tree view
         for i in range(len(data)):
-            tree.insert("", i, text = str(i),  values = (data[i]['Film_ID'], data[i]['Film'], data[i]['Genre'], data[i]['Showtime'], data[i]['Price'], data[i]['Stock']))
+            tree.insert("", i, text = str(i),  values = (data[i]['Film id'], data[i]['Film'], data[i]['Genre'], data[i]['Showtime'], data[i]['Price'], data[i]['Stock']))
