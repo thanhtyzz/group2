3573d29859f55aecaa2a632af8f4d24b4f0bd40e
DuyenAn <antvd22411c@st.uel.edu.vn>
2023-05-08 11:33:49 +0700

    HHuhuh

--- Modules/User/Component/Buytickets/User_Buytickets_create.py
+++ Modules/User/Component/Buytickets/User_Buytickets_create.py
@@ -112,6 +112,7 @@ class User_Buytickets_create:
 
         obj.entry_image_1 = PhotoImage(file=assets_path / "Textbox_1.png")
         obj.entry_image_2 = PhotoImage(file=assets_path / "Textbox_2.png")
+        
 
         obj.entry_bg_1 = obj.canvas.create_image(231.0, 459.0, image=obj.entry_image_1)
         obj.entry_1 = Entry(obj.canvas, textvariable=obj.film_id, bd=0, bg="#D9D9D9", fg="#000716", highlightthickness=0)
@@ -128,6 +129,10 @@ class User_Buytickets_create:
         obj.entry_bg_4 = obj.canvas.create_image(231.0, 354.0, image=obj.entry_image_1)
         obj.entry_4 = Entry(obj.canvas, textvariable = obj.price, bd=0, bg="#D9D9D9", fg="#000716", highlightthickness=0)
         obj.entry_4.place(x=146.0, y=448.0, width=170.0, height=20.0)
+
+        obj.entry_bg_1 = obj.canvas.create_image(231.0, 459.0, image=obj.entry_image_1)
+        obj.entry_1 = Entry(obj.canvas, textvariable=obj.film_id, bd=0, bg="#D9D9D9", fg="#000716", highlightthickness=0)
+        obj.entry_1.place(x=146.0, y=343.0, width=170.0, height=20.0)
 
     @staticmethod
     def generate_buytickets_buttons(obj):
